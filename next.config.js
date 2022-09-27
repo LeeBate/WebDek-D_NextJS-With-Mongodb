b55e342ff2ea70437d270d3effdebe1b2194b0ca
NATTAPHONG WONGSAENGPHET <Harukaxd02@gmail.com>
2022-09-27 17:00:11 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+module.exports = {
+  env: {
+      "BASE_URL": "http://localhost:3000",
+      "MONGODB_URL": "mongodb+srv://AdminX:admin123@project.ljdx6ja.mongodb.net/?retryWrites=true&w=majority",
+      "ACCESS_TOKEN_SECRET": "project48615324896761524",
+      "REFRESH_TOKEN_SECRET": "project77759664823511548226",
+      "PAYPAL_CLIENT_ID": "AS5QhaJ6fnAQNiaJqrjUe9nBvEvTivhkK5vR2Gtg271vXVFUKDBMhiWGBLADB7AjskKHc2gWrcEudo01",
+      "CLOUD_UPDATE_PRESET": "my-uploads",
+      "CLOUD_NAME": "dvktk1a8x",
+      "CLOUD_API": "https://api.cloudinary.com/v1_1/dvktk1a8x/image/upload"
+  }
 }
-
-module.exports = nextConfig
