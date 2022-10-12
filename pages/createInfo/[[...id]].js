c80f953aa40f368a107c569ca1f31c10f9358ab7
phanumat44 <96937316+phanumat44@users.noreply.github.com>
2022-10-12 21:21:04 +0700

    แก้editข่าวสารและลองเปลี่ยนฟ้อน

--- pages/createInfo/[[...id]].js
+++ pages/createInfo/[[...id]].js
@@ -124,7 +124,7 @@ const NewsManager = () => {
       <form method='post' onSubmit={handleSubmit}  className="space-y-8">
           <div>
               <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-800">หัวเรื่อง</label>
-              <input type="text" name='title' className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 
+              <input value={title} type="text" name='title' className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 
               text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500
               block w-full p-2.5 "onChange={handleChangeInput} placeholder="กรอกหัวเรื่อง . . . " required/>
           </div>
